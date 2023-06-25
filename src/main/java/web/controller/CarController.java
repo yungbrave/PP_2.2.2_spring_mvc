7ec2c0a7ac8ca67f449acb7fb2eb9ab4fdc41839
yungbrave <SergeyIlinR@gmail.com>
2023-06-26 01:28:11 +0300

    Dependency Injection using Spring

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,16 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping("/cars")
     public String showCar(@RequestParam("carNum") int carNum,
                           Model model) {
-        CarService carService = new CarService();
         List<Car> cars = carService.getCarsByNum(carNum);
         model.addAttribute("cars", cars);
         return "cars";
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,11 +1,13 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.Car;
 import web.dao.CarDao;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class CarService {
 
     public List<Car> getCarsByNum(int carNum) {
